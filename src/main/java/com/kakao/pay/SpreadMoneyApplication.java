63fd0d13e6601201bc7d29a4861950a3ba589b79
Eunji Song <ijaysong0811@gmail.com>
2020-06-25 23:21:33 +0900

    첫번째 커밋

---
+++ src/main/java/com/kakao/pay/SpreadMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadMoneyApplication.class, args);
+	}
+
+}
